c018df38dbb80509124d2541983959b8beb915ac
zacbarton <zacbarton@gmail.com>
2014-07-11 08:53:16 +1000

    allow undoing of the last deleted todo

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,49 @@
 angular.module("Demo", ["ngStorage", "zacbarton.bindPolymer"])
-	.config(function($locationProvider) {
-		$locationProvider.html5Mode(true);
-	})
-	.run(function($rootScope, $location) {
-		// watch for polymer url changes
-		$rootScope.$watch(function(){ return $location.hash(); }, function(value) {
-			$rootScope.view = value;
-		});
-	})
-	.controller("Todo", function($scope, $timeout, $sessionStorage) {
-		$scope.storage = $sessionStorage.$default({
-			items: []
-		});
-		
-		$scope.input = "";
-		$scope.icon = "add";
-		$scope.items = $scope.storage.items;
-		
-		$scope.add = function() {
-			if ($scope.input === "") {
-				return;
-			}
-			
-			$scope.icon = "check";
-			$timeout(function() {
-				$scope.icon = "add";
-			}, 750);
-			
-			$scope.storage.items.unshift({done: false, text: $scope.input, timestamp: Date.now()});
-			$scope.input = "";
-		};
-		
-		$scope.edit = function(item, active) {
-			item.edit = active;
-		};
-		
-		$scope.remove = function(item) {
-			$scope.storage.items.splice($scope.storage.items.indexOf(item), 1);
-		};
+
+.config(function($locationProvider) {
+	$locationProvider.html5Mode(true);
+})
+
+.run(function($rootScope, $location) {
+	// watch for polymer url changes
+	$rootScope.$watch(function(){ return $location.hash(); }, function(value) {
+		$rootScope.view = value;
 	});
+})
+
+.controller("Todo", function($scope, $timeout, $sessionStorage) {
+	$scope.storage = $sessionStorage.$default({
+		items: []
+	});
+	
+	$scope.input = "";
+	$scope.icon = "add";
+	
+	$scope.add = function() {
+		if ($scope.input === "") {
+			return;
+		}
+		
+		$scope.icon = "check";
+		$timeout(function() {
+			$scope.icon = "add";
+		}, 750);
+		
+		$scope.storage.items.unshift({done: false, text: $scope.input, timestamp: Date.now()});
+		$scope.input = "";
+	};
+	
+	$scope.edit = function(item, active) {
+		item.edit = active;
+	};
+	
+	$scope.remove = function(item) {
+		var undoItem = $scope.storage.items.splice($scope.storage.items.indexOf(item), 1);
+		$scope.undoItem = undoItem[0];
+	};
+	
+	$scope.undo = function() {
+		$scope.storage.items.unshift($scope.undoItem);
+		$scope.undoItem = null;
+	};
+});
